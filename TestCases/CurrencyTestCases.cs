b7c2798fc00896cdce3d16e18ec5ec63c2570b07
Inderpreet Singh <inderpreet.singh@u.nud.edu>
2022-04-13 14:28:24 +0530

    adding sample test cases

--- TestCases/CurrencyTestCases.cs
+++ TestCases/CurrencyTestCases.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace PlanoCurrency.TestCases
 {
     public class CurrencyTestCases
     {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1() => Assert.AreEqual(1, 1);
     }
 }
